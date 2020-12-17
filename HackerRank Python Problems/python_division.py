a74b64704e9bfebe00ae54137bbc73c5c5ed2305
Samarpan Dasgupta <samarpan2dasgupta@gmail.com>
2020-12-17 13:50:46 +0530

    Add files via upload

---
+++ HackerRank Python Problems/python_division.py
@@ -0,0 +1,10 @@
+# The first line contains the first integer, .
+# The second line contains the second integer, .
+
+# Print the two lines as described above.
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
